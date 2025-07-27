37fb7ade134f3879724765371ef3a5c5b7bf345d
YukiChriss <naoyuki.christopher.higaki@outlook.com>
2025-07-27 15:03:23 +0200

    Add project files.

---
+++ string-interpolation-demo-cs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_interpolation_demo_cs
+{
+    // String Interpolation: 
+    // Allows for easier and more readable string formatting.
+    // $: Indicates that the string is an interpolated string.
+    // {}: Used to include expressions within the string.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+        }
+    }
+}
---
+++ string-interpolation-demo-cs/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_interpolation_demo_cs
+{
+    class Solution
+    {
+        public Solution()
+        {
+            String FirstName = "Yuki";
+            String LastName = "James";
+            int Age = 33;
+
+            Console.WriteLine($"Hello There.");
+            Console.WriteLine($"My name is {LastName}.");
+            Console.WriteLine($"{FirstName} {LastName}.");
+            Console.WriteLine($"I am {Age} years old.");
+        }
+    }
+}
